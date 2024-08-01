90fc70c1776c04420ecb333106970320a08fa444
Dranee <adrianaviladiaz93@gmail.com>
2024-08-01 12:30:32 -0500

    update dashboard tab

--- containers/dashboard/components/tabs.tsx
+++ containers/dashboard/components/tabs.tsx
@@ -1,3 +1,4 @@
+import useDeviceInfo from '@/hooks/useDeviceInfo';
 import { cn } from '@/lib/utils';
 import { ArchiveIcon, CheckIcon, DramaIcon } from 'lucide-react';
 import React from 'react';
@@ -13,16 +14,25 @@ export const TABS = {
 };
 
 const TabsDashboard = (props: IProps) => {
+  const device = useDeviceInfo();
+  console.log(device);
+  const isBrowser = device?.deviceType === 'browser';
   return (
-    <header className='fixed inset-x-0 bottom-0 z-50 grid w-full max-w-2xl grid-cols-2 mx-auto bg-black h-9'>
+    <header
+      className={cn(
+        'fixed inset-x-0 bottom-0 z-50 grid w-full h-12 max-w-2xl grid-cols-2 mx-auto bg-black',
+        isBrowser ? 'h-9' : 'h-12'
+      )}
+    >
       {Object.entries(TABS).map(([k, v]) => (
         <article
           key={v.value}
           className={cn(
-            'flex text-sm text-center bg-white/10 justify-center items-center gap-2',
+            'flex text-sm text-center bg-white/10 justify-center gap-2',
             props.tabSelected === v.value
               ? 'bg-white/20'
-              : 'cursor-pointer text-white/30'
+              : 'cursor-pointer text-white/30',
+            isBrowser ? 'items-start pt-2 ' : 'items-center'
           )}
           onClick={() => props.setTabSelected(v.value)}
         >
