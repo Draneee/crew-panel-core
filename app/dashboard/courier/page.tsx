adf04383a7670e81b80f0687493d43786193c76a
Dranee <adrianaviladiaz93@gmail.com>
2024-08-29 16:24:56 -0500

    chore: Refactor CourierPage component for improved code readability and error handling

--- app/dashboard/courier/page.tsx
+++ app/dashboard/courier/page.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import CourierContainer from '@/containers/dashboard/courier/page';
-import { supabase } from '@/lib/supabase/client';
+
 import { redirect } from 'next/navigation';
 import { USER_BLOCK } from '@/consts/user-block';
+import { createClient } from '@/lib/supabase/server';
 
 const CourierPage = async () => {
+  const supabase = createClient();
   const {
     data: { user },
-  } = await supabase.auth.getUser();
-
-  if (!user) redirect('/dashboard');
+  } = await supabase.auth.getUser().then((res) => {
+    if (!res) {
+      redirect('/dashboard');
+    }
+    return res;
+  });
 
   const haveRestriction = USER_BLOCK[user?.email ?? ''] ? true : false;
-  if (haveRestriction && user.email) {
+  if (haveRestriction && user?.email) {
     const { layout } = USER_BLOCK?.[user.email];
     if (layout.courier) {
       return <CourierContainer />;
